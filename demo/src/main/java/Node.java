3cce1506cc788261eed9528895eb078e8e2f5f8f
HKoenig6 <hkoenig@wisc.edu>
2022-06-14 18:25:26 -0500

    basic functionality for graph construction, WIP

--- demo/src/main/java/Node.java
+++ demo/src/main/java/Node.java
@@ -2,9 +2,45 @@ import java.util.*;
 
 public class Node {
 
-    Dictionary<String,Integer> prerequisites;
+    private Dictionary<String,String> recipe;
+    private Dictionary<String,Float> edges;
 
-    public Node () {
+    public Node (Dictionary<String,String> recipe) {
 
+        // culls empty info from CSV
+        Enumeration<String> keys = recipe.keys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            try {
+                if (Integer.valueOf(recipe.get(key)) == 0) 
+                    recipe.remove(key);
+                } catch (NumberFormatException N) {
+                    // allow non-integer error to pass
+            }
+        }
+
+        this.recipe = recipe;
+        this.edges = new Hashtable<String,Float>();
+    }
+
+    public Dictionary<String,String> getInfo() {
+        return this.recipe;
+    }
+
+    public void addEdge(String material, Float rate) {
+        edges.put(material, rate);
+    }
+
+    public float getModifier() {
+        int edgeSum = 0;
+        Enumeration<Float> values = edges.elements();
+        while (values.hasMoreElements()) {
+            edgeSum += values.nextElement();
+        }
+        return edgeSum / Float.valueOf(recipe.get("trate"));
+    }
+
+    public Dictionary<String, Float> getEdges() {
+        return edges;
     }
 }
--- demo/src/main/java/Optimizer.java
+++ demo/src/main/java/Optimizer.java
@@ -1,12 +1,106 @@
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+//import org.apache.commons.lang3.SerializationUtils;
 import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
 
 public class Optimizer {
+
+    private static void addNode(Dictionary<String,Node> graph, 
+    Dictionary<String,Dictionary<String,String>> recipes, Enumeration<String> materials,
+    List<String> marked) {
+        String key = "";
+        String graphKey = "";
+        while (materials.hasMoreElements()) {
+            try {              
+                key = materials.nextElement();
+                //short circuits all rates as they are currently not considered
+                Integer.valueOf(key);
+                //continue
+            } catch (NumberFormatException N) {
+                Dictionary<String,String> recipe = recipes.get(formatLabel(key, true));
+                if (recipe == null) continue;
+                Node nextNode = new Node(recipe);                  
+                graphKey = formatLabel(key, false);
+                graph.put(graphKey, nextNode);
+                marked.add(key);
+                Enumeration<String> prerequisites = nextNode.getInfo().elements();
+                addNode(graph, recipes, prerequisites, marked);
+            }
+        }
+    }
+
+    private static String formatLabel(String oldLabel, boolean append) {
+        if (!append) {
+            return oldLabel.contains("_1") ||
+            oldLabel.contains("_2") || 
+            oldLabel.contains("_3") || 
+            oldLabel.contains("_4") ? oldLabel.substring(0,oldLabel.length()-2) : oldLabel;
+        } else {
+            switch (oldLabel.substring(0, oldLabel.length()-2)) {
+                case "_1":
+                case "_2":
+                case "_3":
+                case "_4":
+                    return oldLabel;
+                default:
+                    //TODO implement this string to be flexible with what recipe is used
+                    return oldLabel + "_1";
+            }
+        }
+    }
+
+    private static void findRates(Dictionary<String,Node> graph, List<String> marked, 
+    List<String> targets, String material) {
+        // if this material is a prerequisite of another in the graph, do that material first
+        // and store all relevant rates in this local function
+        Dictionary<String,Float> allRates = new Hashtable<String,Float>();
+        Enumeration<String> keyList = graph.keys();
+        while (keyList.hasMoreElements()) {
+            String graphKey = keyList.nextElement();
+            Node currMaterial = graph.get(graphKey);
+            Dictionary<String,String> recipe = currMaterial.getInfo();
+
+            Enumeration<String> entries = recipe.elements();
+            Enumeration<String> keys = recipe.keys();
+            String entry = "";
+            String key = "";
+            while (entries.hasMoreElements()) {
+                try {
+                    entry = entries.nextElement();
+                    key = keys.nextElement();
+                    Integer.valueOf(entry);
+                    //continue
+                } catch (NumberFormatException N) {
+                    // run findRates on a node that requires this material before running
+                    // on this material
+                    if (material.equals(entry) && marked.contains(graphKey)) {
+                        findRates(graph, marked, targets, graphKey);
+                    }
+                    if (material.equals(entry)) {
+
+                        System.out.println(graphKey);
+                        System.out.println(currMaterial.getModifier() + "\n");
+
+                        allRates.put(graphKey, Integer.valueOf(recipe.get("prate" + key.substring(key.length()-1, key.length()))) * currMaterial.getModifier());
+                    }
+                }
+            }
+        }
+        Enumeration<String> rateKeys = allRates.keys();
+        Node currNode = graph.get(material);
+        while (rateKeys.hasMoreElements()) {
+            String rateKey = rateKeys.nextElement();
+            currNode.addEdge(rateKey, allRates.get(rateKey));
+        }
+        marked.remove(material);
+    }
 
     public static void main(String[] args) {
         try {
             Dictionary<String, Node> graph = new Hashtable<String,Node>();
+            List<String> targets = new ArrayList<String>();
 
             //convert CSV to 2D dictionary
             CSVReader csvReader = new CSVReaderHeaderAware(new FileReader("recipes.csv"));
@@ -21,15 +115,49 @@ public class Optimizer {
                 }
             }
 
+            List<String> marked = new ArrayList<String>();
+
             //TODO edit this later to accomodate for custom rates and alternate recipes
-            for (String node:args) {
-                if (node.equals(args[0])) continue;
-                graph.put(node, new Node());
+            for (String material: args) {
+                Dictionary<String,String> recipe = recipes.get(material);
+                Node currNode = new Node(recipe);
+                String formattedMaterial = formatLabel(material, false); 
+                graph.put(formattedMaterial, currNode);
+                marked.add(formattedMaterial);
+                targets.add(formattedMaterial);
+                //TODO custom rate area of concern
+                currNode.addEdge("Target", Float.valueOf(recipe.get("trate")));
             }
 
-        } catch (Exception E) {
-            System.err.println(E);
+            //first, add all necessary nodes
+            for (String key: targets) {   
+                Enumeration<String> prerequisites = graph.get(key).getInfo().elements();
+                addNode(graph, recipes, prerequisites, marked);
+            }
+
+            //add all edges and their respective rates
+            while(!marked.isEmpty()) {
+                try {
+                    Enumeration<String> iter = Collections.enumeration(marked);
+                    while (iter.hasMoreElements()) {
+                        findRates(graph, marked, targets, iter.nextElement());
+                    }
+                } catch (ConcurrentModificationException CM) {
+                    //i dont think thisll work but lets try :)
+                }
+            }          
+
+            Enumeration<String> test = graph.keys();
+            while(test.hasMoreElements()) {
+                String material = test.nextElement();
+                System.out.println("\n==== " + material + " ====");
+                System.out.println(graph.get(material).getEdges());
+            }
+
+        } catch (IOException I) {
+                System.err.println(I);
+        } catch (CsvException C) {
+                System.err.println(C);
         }
     }
 }
-
